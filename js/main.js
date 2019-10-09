75c69b15c0d7df545bf5b4ec676c623f7dcdef9b
dk8188 <dkvochko2011@yahoo.co.jp>
2019-10-09 23:53:19 +0900

    task 4

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 
 'use strict';
-var AVATAR_PHOTOS = ['01', '02', '03', '04', '05', '06', '07', '08'];
 
-var OFFER_TITLES = ['сдам квартиру', 'уютная квартира', 'просторный пентхаус',
-  'только славянам', 'квартира в доме с паркингом', 'квартира в самом центре',
-  'квартира в самом центре города', 'квартира с мансардой'];
+var OFFER_TITLES = [
+  'сдам квартиру',
+  'уютная квартира',
+  'просторный пентхаус',
+  'только славянам',
+  'квартира в доме с паркингом',
+  'квартира в самом центре',
+  'квартира в самом центре города',
+  'квартира с мансардой'
+];
 
 var APARTAMENT_TYPE = ['пентаус', 'квартира', 'дом', 'бунгало', 'апартаменты', 'общежитие'];
 var CHECKIN_TIME = ['12:00', '13:00', '14:00'];
@@ -17,24 +23,42 @@ var APARTAMENT_PHOTO = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-var ENTER_KEYCODE = 13;
 
 // получение DOM элементов
-
-document.addEventListener('mousedown', function () {
-  var mapActivate = document.querySelector('.map');
-  mapActivate.classList.remove('map--faded');
-});
-
 var map = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content;
 var pin = pinTemplate.querySelector('.map__pin');
-// функция получения рандомного элемента из массива
+var mainPin = document.querySelector('.map__pin--main');
+var form = document.querySelector('.ad-form');
+
+mainPin.addEventListener('mousedown', function () {
+  map.classList.remove('map--faded'); // re-write
+  form.classList.remove('ad-form--disabled');
+  mapPins.prepend(placeAdsOnTheMap);
+  generateCard();
+});
+var onMouseMove = function (moveEvt) {
+   moveEvt.preventDefault();
+   dragged = true;
+var shift = {
+  x: startCoords.x - moveEvt.clientX,
+  y: startCoords.y - moveEvt.clientY
+};
+startCoords = {
+  x:moveEvt.clientX,
+  y:
+};
+}
+/**
+ * функция получения рандомного элемента из массива
+ * @param {Array} arr - изначальный массив
+ * @return {*} - рандомный элемент массива
+ */
 var getRandomElement = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
-// функция получения рандомгного числа в заданном интервале
+// функция получения рандомного числа в заданном интервале
 // сортитровка значений
 var getRandomNumber = function (max, min) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -59,14 +83,14 @@ var generateRandomArray = function (arr) {
 var crateSimilarAdd = function () {
   var positionX = getRandomNumber(25, map.clientHeight - 50); // 25, -25 is used in a way so that pin doest cross the broder
   var positionY = getRandomNumber(160, 530);
-  var ad = {
+  return {
     'author': {
-      'avatar': 'img/avatars/user' + getRandomElement(AVATAR_PHOTOS) + '.png'
+      'avatar': 'img/avatars/user0' + getRandomNumber(1, 8) + '.png'
     },
     'offer': {
       'title': getRandomElement(OFFER_TITLES),
       'type': getRandomElement(APARTAMENT_TYPE),
-      'address': positionX + ',' + positionY,
+      'address': positionX + ', ' + positionY,
       'price': getRandomNumber(1000, 50000),
       'guests': getRandomNumber(1, 6),
       'rooms': getRandomNumber(1, 6),
@@ -80,49 +104,49 @@ var crateSimilarAdd = function () {
       'y': positionY
     }
   };
-  return ad;
 };
 // генерация массива с объктами данных
 var generateAds = function (count) {
-  var arr = [];
+  var ads = [];
   for (var i = 0; i < count; i++) {
-    arr.push(crateSimilarAdd());
+    ads.push(crateSimilarAdd());
   }
-  return arr;
+  return ads;
 };
-var ads = generateAds(8);
+var ads = generateAds(8); // const
 
 
-var generatePin = function (element) {
+var generatePin = function (pinData) {
   var clonePin = pin.cloneNode(true);
   var pinImg = clonePin.querySelector('img');
-  clonePin.style.left = element.location.x + 50 + 'px';
-  clonePin.style.top = element.location.y + 75 + 'px';
-  pinImg.image = element.author.avatar;
-  pinImg.alt = element.author.title;
+  clonePin.style.left = pinData.location.x - 25 + 'px'; // const
+  clonePin.style.top = pinData.location.y - 70 + 'px'; // const
+  pinImg.src = pinData.author.avatar;
+  pinImg.alt = pinData.author.title;
   return clonePin;
 };
-var createAdsFromArray = function (dataArray) {
-  var fragment = new DocumentFragment();
-  for (var i = 0; i < dataArray.length; i++) {
+var renderPinsFromArray = function (dataArray) { // TODO rename to pinsData
+  var fragment = new DocumentFragment(); // TODO - documnent.createDocumentFragment
+  for (var i = 0; i < dataArray.length; i++) { // TODO for each
     fragment.prepend(generatePin(dataArray[i]));
   }
   return fragment;
 };
 
-var placeAdsOnTheMap = createAdsFromArray(ads);
-mapPins.prepend(placeAdsOnTheMap);
+var placeAdsOnTheMap = renderPinsFromArray(ads);
+
 
 var generateCard = function () {
-  var ad1 = ads[0];
-  var card = document.querySelector('#card').content;
-  var cardAvatar;
+  var ad1 = ads[1];
+  var card = document.querySelector('#card').content; // clone
+  var cardAvatar = card.querySelector('.popup__avatar');
   var cardTitle = card.querySelector('.popup__title');
   var cardAddress = card.querySelector('.popup__text--address');
   var cardPrice = card.querySelector('.popup__text--price');
   var cardType = card.querySelector('.popup__type');
   var cardCapacity = card.querySelector('.popup__text--capacity');
 
+  cardAvatar.content = ad1.offer.avatar;
   cardTitle.textContent = ad1.offer.title;
   cardAddress.textContent = ad1.offer.address;
   cardPrice.textContent = ad1.offer.price + ' RUB';
@@ -131,5 +155,3 @@ var generateCard = function () {
   var mapPoint = map.querySelector('.map__filters-container');
   map.insertBefore(card, mapPoint);
 };
-generateCard();
-
